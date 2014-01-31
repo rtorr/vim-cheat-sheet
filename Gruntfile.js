7c4f55e295582639e6c6b613480b69980012973b
Richard Torruellas <rtorruellas@gmail.com>
2014-01-31 00:31:38 -0700

    removing tools that were not being used

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,18 +25,6 @@ module.exports = function (grunt) {
   grunt.initConfig({
     yeoman: yeomanConfig,
     watch: {
-      coffee: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.coffee'],
-        tasks: ['coffee:dist']
-      },
-      coffeeTest: {
-        files: ['test/spec/{,*/}*.coffee'],
-        tasks: ['coffee:test']
-      },
-      compass: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-        tasks: ['compass:server']
-      },
       livereload: {
         options: {
           livereload: LIVERELOAD_PORT
@@ -122,75 +110,6 @@ module.exports = function (grunt) {
         options: {
           run: true,
           urls: ['http://localhost:<%= connect.options.port %>/index.html']
-        }
-      }
-    },
-    coffee: {
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: '<%= yeoman.app %>/scripts',
-            src: '{,*/}*.coffee',
-            dest: '.tmp/scripts',
-            ext: '.js'
-          }
-        ]
-      },
-      test: {
-        files: [
-          {
-            expand: true,
-            cwd: 'test/spec',
-            src: '{,*/}*.coffee',
-            dest: '.tmp/spec',
-            ext: '.js'
-          }
-        ]
-      }
-    },
-    compass: {
-      options: {
-        sassDir: '<%= yeoman.app %>/styles',
-        cssDir: '.tmp/styles',
-        generatedImagesDir: '.tmp/images/generated',
-        imagesDir: '<%= yeoman.app %>/images',
-        javascriptsDir: '<%= yeoman.app %>/scripts',
-        fontsDir: '<%= yeoman.app %>/styles/fonts',
-        importPath: '<%= yeoman.app %>/bower_components',
-        httpImagesPath: '/images',
-        httpGeneratedImagesPath: '/images/generated',
-        httpFontsPath: '/styles/fonts',
-        relativeAssets: false
-      },
-      dist: {},
-      server: {
-        options: {
-          debugInfo: true
-        }
-      }
-    },
-    // not used since Uglify task does concat,
-    // but still available if needed
-    /*concat: {
-     dist: {}
-     },*/
-    requirejs: {
-      dist: {
-        // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
-        options: {
-          // `name` and `out` is set by grunt-usemin
-          baseUrl: yeomanConfig.app + '/scripts',
-          optimize: 'none',
-          // TODO: Figure out how to make sourcemaps work with grunt-usemin
-          // https://github.com/yeoman/grunt-usemin/issues/30
-          //generateSourceMaps: true,
-          // required to support SourceMaps
-          // http://requirejs.org/docs/errors.html#sourcemapcomments
-          preserveLicenseComments: false,
-          useStrict: true,
-          wrap: true
-          //uglify2: {} // https://github.com/mishoo/UglifyJS2
         }
       }
     },
@@ -285,6 +204,15 @@ module.exports = function (grunt) {
     // Put files not handled in other tasks here
     copy: {
       dist: {
+        styles: {
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '.tmp/',
+          src: [
+            'styles/{,*/}*.css',
+            'bower_components/boostrap/dist/css/bootstrap.min.css'
+          ]
+        },
         files: [
           {
             expand: true,
@@ -311,15 +239,11 @@ module.exports = function (grunt) {
     },
     concurrent: {
       server: [
-        'compass',
-        'coffee:dist'
+
       ],
       test: [
-        'coffee'
       ],
       dist: [
-        'coffee',
-        'compass',
         'imagemin',
         'svgmin',
         'htmlmin'
