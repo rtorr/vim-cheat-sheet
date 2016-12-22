22f59f839ee9b9f00ee5102db1bac9b474d75ab2
Richard Torruellas <richard.torruellas@am.sony.com>
2016-12-22 11:15:58 -0800

    make it installable

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+---
+permalink: service-worker.js
+---
+
+"use strict";
+
+console.log('WORKER: executing.');
+
+var version = 'v1.0.2';
+var pathsToCache = [
+  '/',
+  '/lang/pt_br/',
+  '/lang/de_de/',
+  '/lang/id/',
+  '/lang/es_es/',
+  '/lang/tr/',
+  '/lang/th/',
+  '/lang/zh_tw/',
+  '/lang/ko/',
+  '/lang/ru/',
+  '/lang/zh_cn/',
+  '/lang/it/',
+  '/lang/fr_fr/',
+  '/lang/uk/',
+  '/lang/nl_nl/',
+  '/lang/ja/',
+  '/lang/ro/',
+  '/code_of_conduct/'
+];
+
+self.addEventListener('install', event => {
+  console.log('WORKER: install event in progress.');
+  event.waitUntil(
+    caches
+      .open(version)
+      .then(cache => cache.addAll(pathsToCache))
+      .then(_ => console.log('WORKER: install completed'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('WORKER: activate event in progress.');
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys => Promise.all(keys.filter(key => !key.startsWith(version)).map(key => caches.delete(key))))
+      .then( _ => console.log('WORKER: activate completed.'))
+  );
+});
