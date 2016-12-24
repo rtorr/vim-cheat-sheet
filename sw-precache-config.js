ebd609cee1893bb2a92a5156d672227ae382ebcf
Richard <rtorruellas@gmail.com>
2016-12-24 02:08:10 -0800

    sw

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,4 +1,60 @@
 module.exports = {
+  dynamicUrlToDependencies: {
+    '/': [
+      '_site/index.html'
+    ],
+    '/lang/pt_br/': [
+      '_site/lang/pt_br/index.html'
+    ],
+    '/lang/de_de/': [
+      '_site/lang/de_de/index.html'
+    ],
+    '/lang/id/': [
+      '_site/lang/id/index.html'
+    ],
+    '/lang/es_es/': [
+      '_site/lang/es_es/index.html'
+    ],
+    '/lang/tr/': [
+      '_site/lang/tr/index.html'
+    ],
+    '/lang/th/': [
+      '_site/lang/th/index.html'
+    ],
+    '/lang/zh_tw/': [
+      '_site/lang/zh_tw/index.html'
+    ],
+    '/lang/ko/': [
+      '_site/lang/ko/index.html'
+    ],
+    '/lang/ru/': [
+      '_site/lang/ru/index.html'
+    ],
+    '/lang/zh_cn/': [
+      '_site/lang/zh_cn/index.html'
+    ],
+    '/lang/it/': [
+      '_site/lang/it/index.html'
+    ],
+    '/lang/fr_fr/': [
+      '_site/lang/fr_fr/index.html'
+    ],
+    '/lang/uk/': [
+      '_site/lang/uk/index.html'
+    ],
+    '/lang/nl_nl/': [
+      '_site/lang/nl_nl/index.html'
+    ],
+    '/lang/ja/': [
+      '_site/lang/ja/index.html'
+    ],
+    '/lang/ro/': [
+      '_site/lang/ro/index.html'
+    ],
+    '/code_of_conduct/': [
+      '_site/code_of_conduct/index.html'
+    ]
+  },
   staticFileGlobs: [
     '_site/**/*.html',
     '_site/**/*.png'
