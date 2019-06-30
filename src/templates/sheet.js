2ef804ff4656512e9ffabcad6aa6d32b958024b1
jkaichang <jkaichang@users.noreply.github.com>
2019-06-30 23:04:16 +0800

    Fix rtorr/vim-cheat-sheet#260. Adding query fields.

--- src/templates/sheet.js
+++ src/templates/sheet.js
@@ -91,6 +91,9 @@ export const query = graphql`
               comma
               CtrlPluse
               CtrlPlusy
+              CtrlPlusf
+              CtrlPlusd
+              CtrlPlusu
               zz
             }
           }
