ee83492a5f07a8ad36a6cfc99314766be8bada84
Richard Torruellas <rtorruellas@gmail.com>
2019-11-09 10:41:55 -0800

    sw remove cache

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/javascript/sw.js');
+    navigator.serviceWorker.register('/javascripts/sw.js');
   });
 }
