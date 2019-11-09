6ff855908a587f7f0a60e2e3b4b3e91f3a151935
Richard Torruellas <rtorruellas@gmail.com>
2019-11-09 10:51:04 -0800

    sw remove cache

--- public/javascripts/sw.js
+++ public/javascripts/sw.js
@@ -4,9 +4,7 @@ self.addEventListener('activate', function(event) {
       return Promise.all(
         cacheNames
           .filter(function(cacheName) {
-            // Return true if you want to remove this cache,
-            // but remember that caches are shared across
-            // the whole origin
+            return true;
           })
           .map(function(cacheName) {
             return caches.delete(cacheName);
