40d1cd26d6d5bb1a8110adb7796481cbcae666fa
Richard <297045+rtorr@users.noreply.github.com>
2019-11-11 20:15:42 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('index', {
+    locale: 'en',
     languagesMetaData: req.app.get('languagesMetaData'),
     version: req.app.get('packageVersion')
   });
