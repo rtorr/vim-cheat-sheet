097a140151fdd178ab8bb333ab55c864957069fc
Richard Torruellas <rtorruellas@gmail.com>
2019-11-11 20:45:04 -0800

    remove unneeded change

--- locales.js
+++ locales.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 const directory = __dirname + '/locales';
 const localesFiles = fs.readdirSync(directory);
 const locales = localesFiles.map(fileName => fileName.split('.')[0]);
-const enUs = JSON.parse(fs.readFileSync(`${directory}/en_us.json`));
 
 const metaData = localesFiles.map(fileName => {
   const { id, title, lang } = JSON.parse(
